e188311348c0ef6e526e7cf89fc887fbf751da96
lucerio <lucero.develops@gmail.com>
2017-08-17 23:03:45 -0400

    made finally touches on styling ran code through validators

--- js/app.js
+++ js/app.js
@@ -1,66 +1,62 @@
-'use strict';
-
-//Map 
-var mapTitle = 'Indianapolis, IN';
+"use strict";
+//Map
+var mapTitle = "Indianapolis, IN";
 var mapCordinates = {
   lat: 39.764854,
   lng: -86.168751
 };
 
 //Using FOURSQUARE for the API req
-var fourSqHelper = 'https://api.foursquare.com/v2/venues/search?ll=';
-var clientID = 'J10A2OQSTX4OYMJ2U2SSANP5BQGWAXMKOLJOKJUU4RTUFCBA';
-var clientSecret = '5NTWSWTPJDGTXLG0ELGXP52CFU0UB0HL2GMJVANKDALJKHM0';
+var fourSqHelper = "https://api.foursquare.com/v2/venues/search?ll=";
+var clientID = "J10A2OQSTX4OYMJ2U2SSANP5BQGWAXMKOLJOKJUU4RTUFCBA";
+var clientSecret = "5NTWSWTPJDGTXLG0ELGXP52CFU0UB0HL2GMJVANKDALJKHM0";
 
-
-//var bounds;
-
-var mapZoom = 12;
+var mapZoom = 13;
 var infoWindow;
-//User's search input 
-var searchInput = ko.observable('');
+//User"s search input
+var searchInput = ko.observable("");
 
 //Markers
 var locations = [{
-    title: 'Thristy Scholar',
-    category: 'Coffee',
+    title: "Thristy Scholar",
+    category: "Coffee",
     gCordinates: {
       lat: 39.788438,
       lng: -86.155518
     },
-    vCordinates: '39.788438,-86.155518'
+    vCordinates: "39.788438,-86.155518"
   }, {
-    title: 'Yatz',
-    category: 'Restaurant',
+    title: "Yatz",
+    category: "Restaurant",
     gCordinates: {
       lat: 39.779446,
       lng: -86.142430
     },
-    vCordinates: '39.779446,-86.142430'
+    vCordinates: "39.779446,-86.142430"
   }, {
-    title: 'Invoke Studio',
-    category: 'Yoga',
+    title: "Invoke Studio",
+    category: "Yoga",
     gCordinates: {
       lat: 39.781054,
       lng: -86.149882
     },
-    vCordinates: '39.781054,-86.149882'
+    vCordinates: "39.781054,-86.149882"
   }, {
-    title: 'Nada',
-    category: 'Restaurant',
+    title: "Nada",
+    category: "Restaurant",
     gCordinates: {
       lat: 39.765753,
       lng: -86.158530
     },
-    vCordinates: '39.765753, -86.158530'
+    vCordinates: "39.765753, -86.158530"
   }, {
-    title: 'Dorman Street',
-    category: 'Bar',
+    title: "Dorman Street",
+    category: "Bar",
     gCordinates: {
       lat: 39.779312,
       lng: -86.138810
     },
-    vCordinates: '39.779312, -86.138810'
+    vCordinates: "39.779312, -86.138810"
   }
 
 ];
@@ -73,7 +69,7 @@ var Location = function(data, map) {
   this.category = ko.observable(data.category);
   this.gCordinates = ko.observable(data.gCordinates);
   this.vCordinates = ko.observable(data.vCordinates);
-  this.content = '<h3>' + self.title() + '</h3>';
+  this.content = "<h3>" + self.title() + "</h3>";
 
   this.marker = new google.maps.Marker({
     position: self.gCordinates(),
@@ -82,23 +78,21 @@ var Location = function(data, map) {
     title: self.title()
   });
 
+  this.fourSqURL = fourSqHelper + self.vCordinates() + "&client_id=" + clientID + "&client_secret=" + clientSecret + "&v=20170816";
 
-  this.fourSqURL = fourSqHelper + self.vCordinates() + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20170816';
 
-  //
-  //this.bounds = new google.maps.LatLngBounds();
-
-  google.maps.event.addListener(self.marker, 'click', function() {
+  google.maps.event.addListener(self.marker, "click", function() {
     self.toggleBounce();
     self.infoMagic();
   });
 
+
   $.ajax({
     url: self.fourSqURL,
-    dataType: 'json'
+    dataType: "json"
   }).done(function(data) {
-    self.content = '<div class="infoBox"> <a target="_blank" href="https://foursquare.com/v/' + data.response.venues[0].id + '">' +
-      data.response.venues[0].name + '</a>' + '<div>' + data.response.venues[0].location.formattedAddress + '</div></div>';
+    self.content = "<div class=\"infoBox\"> <a target=\"_blank\" href=\"https://foursquare.com/v/" + data.response.venues[0].id + "\">" +
+      data.response.venues[0].name + "</a>" + "<div>" + data.response.venues[0].location.formattedAddress + "</div></div>";
   }).fail(function() {
     alert("FourSquare api has failed");
   });
@@ -122,11 +116,9 @@ var Location = function(data, map) {
     }
   };
 
-
-  //
   //will need map variable for marker.setMap(map)
   //this adds the marker .  marker.setMap(null) removes the
-  //maker . Now  a condictional can be used  on a boolean to control 
+  //maker . Now  a condictional can be used  on a boolean to control
   // removing or adding the marker
 
   this.markerOnMap = ko.computed(function() {
@@ -147,7 +139,6 @@ var Location = function(data, map) {
     } else {
       self.marker.setMap(null);
     }
-
   });
 
 };
@@ -158,12 +149,13 @@ var ViewModel = function() {
   this.mapTitle = ko.observable(mapTitle);
   //capturing the value that InitMap returns
   this.map = initMap();
-  //creating an array list to house location data 
+  //creating an array list to house location data
   this.allLocations = ko.observableArray([]);
   // appends  all location data into the observble Array
   locations.forEach(function(data) {
     var addLocation = new Location(data, self.map);
     self.allLocations.push(addLocation);
+
   });
 
   //Need to have my locations that are in the search Input stored
@@ -180,20 +172,18 @@ var ViewModel = function() {
   this.listClicker = function(ldata, click) {
     ldata.toggleBounce();
     ldata.infoMagic();
-  }
-
-}
+  };
+};
 
 
 
 function initMap() {
-  //I dont have to define map here I can simple use 
-  //return [[expression]]; 
+  //I dont have to define map here I can simple use
+  //return [[expression]];
   //The return statement ends function execution and specifies
-  //a value to be returned to the function caller, which is 
+  //a value to be returned to the function caller, which is
   //the map variable in the ViewModel
-
-  return new google.maps.Map(document.getElementById('map'), {
+  return new google.maps.Map(document.getElementById("map"), {
     center: mapCordinates,
     zoom: mapZoom
   });
